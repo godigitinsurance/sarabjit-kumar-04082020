30a43aafd6e0c130551545a35c0f1ce6fcde1e8f
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-03 11:46:25 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Sarabjit Kumar");
+$("#policyNumber").text("SC92072820");
+$("#startDate").text("04-Aug-2020");
+$("#endDate").text("03-Aug-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB08CS8001");
+$("#make").text("MARUTI SUZUKI");
+$("#model").text("SWIFT");
+$("#vehicalSubType").text("VDI");
+$("#engineNo").text("D13A2317093");
+$("#chassis").text("MA3FHEB1S00603882");
